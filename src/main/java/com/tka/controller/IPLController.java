5c2a21c0ebeeda424a3cb80285aee856b0b4caf1
TukaramAchugatla <achugatlatukaram@gmail.com>
2025-07-11 23:07:53 +0530

    Curd Opertion Task Done In Maven Project

---
+++ src/main/java/com/tka/controller/IPLController.java
@@ -0,0 +1,40 @@
+package com.tka.controller;
+
+import java.util.List;
+
+import com.tka.entity.Player;
+import com.tka.service.IPLService;
+
+public class IPLController {
+
+	public List<Player> getList() {
+		IPLService s=new IPLService();
+		List<Player> l=s.getList();
+		return l;
+	}
+
+	public int Insertdata(Player p) {
+		IPLService s=new IPLService();
+		int l=s.Insertdata(p);
+		return l;
+	}
+
+	public int updatedata(int i, int j) {
+		IPLService s=new IPLService();
+		int l=s.updatedata(i,j);
+		return l;
+	}
+
+	public int deletedata(int i) {
+		IPLService s=new IPLService();
+		int l=s.deletedata(i);
+		return l;
+	}
+
+	public List<Player> BatsmanOrBowler(String string1, String string2) {
+		IPLService s=new IPLService();
+		List<Player> l=s.BatsmanOrBowler(string1,string2);
+		return l;
+	}
+
+}
