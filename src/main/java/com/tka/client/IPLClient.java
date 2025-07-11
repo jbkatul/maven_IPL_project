ce2191089a47cd6e74794b22d7af1823419199c7
atulraut23 <aturaut@gmail.com>
2025-07-11 22:00:37 +0530

    first commit

---
+++ src/main/java/com/tka/client/IPLClient.java
@@ -0,0 +1,14 @@
+package com.tka.client;
+
+public class IPLClient {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the IPL Client Application!");
+		
+//		IPLController iplController = new IPLController();
+//		List<Player> allplayers = iplController.getAllPlayers();
+		
+	}
+
+}
