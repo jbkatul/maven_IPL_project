338ffe019eed3dde858aba8bc3c541092cd3173d
pradipshinde12 <shindepradip543>
2025-07-12 10:37:33 +0530

    Added getPlayersByRuns method in services, Controller, Client

--- src/main/java/com/tka/client/IPLClient.java
+++ src/main/java/com/tka/client/IPLClient.java
@@ -50,6 +50,12 @@ public class IPLClient {
 		
 		List<Player> p=iplController.BatsmanOrBowler("mi","bowler");
 		System.out.println("Player who is in mi Team And Who is Batsman\n" +p);
+		
+
+        // New method: players with runs greater than given number
+        List<Player> topScorers = iplController.getPlayersByRuns(5000);
+        System.out.println("Players with runs greater than 5000:\n" + topScorers);
+
 
 	}
 
