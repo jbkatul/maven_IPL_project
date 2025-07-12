40da7a4b49d7d6238075770aac6437aafcfdc19e
udpatil2 <udhavdugade2@gmail.com>
2025-07-12 18:05:15 +0530

    Updated pom.xml, IPLClient, and IPLController and ipl service

--- src/main/java/com/tka/service/IPLService.java
+++ src/main/java/com/tka/service/IPLService.java
@@ -60,4 +60,17 @@ public class IPLService {
 		return result;
 
     }
+
+    public List<Player> getPlayersStartingWithH() {
+		IPLDao d = new IPLDao();
+		List<Player> allPlayers = d.getList();
+		List<Player> filtered = new ArrayList<>();
+
+		for (Player p : allPlayers) {
+			if (p.getPname().toLowerCase().startsWith("h")) {
+				filtered.add(p);
+			}
+		}
+		return filtered;
+	}
 }
