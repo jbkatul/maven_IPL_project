e07c5eec978ef1312c02c5c76bf9e37523fe8f56
kalekarprem <kalekarpremsai16@gmail.com>
2025-07-12 10:18:14 +0530

    Start  with v

--- src/main/java/com/tka/client/IPLClient.java
+++ src/main/java/com/tka/client/IPLClient.java
@@ -1,56 +1,34 @@
 package com.tka.client;
 
 import java.util.List;
-
 import com.tka.controller.IPLController;
 import com.tka.entity.Player;
 
 public class IPLClient {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("Welcome to the IPL Client Application!");
-		//Fetch all Data From Table
-		IPLController iplController = new IPLController();
-		List<Player> allplayers = iplController.getList();
-		System.out.println(allplayers);
-	
-//InsertData
-		
-//		Players p=new Players(22, 21, "Shreyas Iyer", 3500, 10, "Batsman", "PBSK");
-//		int l=iplController.Insertdata(p);
-//		if(l!=0) {
-//			System.out.println("Data Inserted");
-//		}
-//		else {
-//			System.out.println("Data not Inserted");
-//		}
+        System.out.println("Welcome to the IPL Client Application!");
 
-		//Update data
-		
-//		int l=iplController.updatedata(2200,8);
-//		if(l!=0) {
-//			System.out.println("Data Updated");
-//		}
-//		else {
-//			System.out.println("Data not updated");
-//		}
-		
-		//Delete Data
-		
-//		int l=iplController.deletedata(22);
-//		if(l!=0) {
-//			System.out.println("Data Deleted");
-//		}
-//		else {
-//			System.out.println("Data Not Deleted");
-//		}
-	
-		//batsman/bowler
-		
-		List<Player> p=iplController.BatsmanOrBowler("mi","bowler");
-		System.out.println("Player who is in mi Team And Who is Batsman\n" +p);
+        IPLController iplController = new IPLController();
 
-	}
+     
+        List<Player> allplayers = iplController.getList();
+        System.out.println("All Players:\n" + allplayers);
+ // Get Bowlers from MI
+        List<Player> bowlers = iplController.BatsmanOrBowler("mi", "bowler");
+        System.out.println("\nPlayers in MI Team who are Bowlers:");
+        for (Player player : bowlers) {
+            System.out.println(player);
+        }
 
+      
+        List<Player> vPlayers = iplController.getPlayersByTeamAndNameStart1("mi", 'v');
+        System.out.println("\nPlayers from MI whose names start with 'V':");
+        for (Player player : vPlayers) {
+            System.out.println(player);
+        }
+
+       
+    }
 }
